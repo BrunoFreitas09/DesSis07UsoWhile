0fdafe6deedcb1286fc423afb21c97f3061113d9
Usuario <Usuario@CPD03-M04>
2024-03-26 10:26:00 -0300

    Adicionar arquivos de projeto.

---
+++ DesSis07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesSis07while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cont = 0;
+             while (Cont<=100)
+                {
+                Console.WriteLine("Seu numero:"+ Cont);
+                Cont++;
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
